00850b333959d65352d39350782d569a09c91d35
Roland Richter <Roland.Richter2@gmx.de>
2015-10-14 18:27:19 +0200

    Initial commit

---
+++ PreAmp_withoutClasses/PreAmp_withoutClasses.c
@@ -0,0 +1,17 @@
+/*
+ * PreAmp_withoutClasses.c
+ *
+ * Created: 23.09.2015 10:29:58
+ *  Author: Roland-User
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
